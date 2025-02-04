4c9193f0b22b7f5dc009b6d60402a1d518ed3f47
Lukas <diaslukas19@gmail.com>
2025-02-03 23:56:15 -0300

    add initial Love2D configuration and implement basic physics for ball and ground

---
+++ walk/conf.lua
@@ -0,0 +1,10 @@
+function love.conf(t)
+    t.window.title = "Love2D Project"
+    t.window.width = 650
+    t.window.height = 650
+    t.window.resizable = false
+    t.window.fullscreen = true
+    t.window.fullscreentype = "desktop"
+    t.window.vsync = true
+    t.window.msaa = 0
+end
